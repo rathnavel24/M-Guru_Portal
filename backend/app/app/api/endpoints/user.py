b8f585af6efdfb6d2fe605ce7e06b7a99362e5d9
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-26 17:38:36 +0530

    RBAC

--- backend/app/app/api/endpoints/user.py
+++ backend/app/app/api/endpoints/user.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, BackgroundTasks,Depends
 from backend.app.app.schemas.user_schema import UserSignUp,UserLogin,Paymentmail
 from sqlalchemy.orm import Session
 from backend.app.app.crud.user_crud import SignUpDetails,LoginUser,UserServices
-from backend.app.app.api.deps import get_db
+from backend.app.app.api.deps import get_db, role_required
 from backend.app.app.utils import send_invoice_email
 router = APIRouter(tags=["login"])
 
 @router.post("/signup")
-async def signup(user_data:UserSignUp,db:Session =  Depends(get_db)):
+async def signup(user_data:UserSignUp,db:Session =  Depends(get_db),
+                 current_user=Depends(role_required([1]))):
         try :
             return SignUpDetails(db,user_data).user_signup()
         except Exception as e:
@@ -19,7 +20,8 @@ async def login(data: UserLogin,background_tasks: BackgroundTasks, db: Session =
     return LoginUser(db, data.email, data.password).login(background_tasks)
 
 @router.post("/payment_email") #send payment email to student
-async def payment_mail(data:Paymentmail,bgtask:BackgroundTasks,db:Session = Depends(get_db)):
+async def payment_mail(data:Paymentmail,bgtask:BackgroundTasks,db:Session = Depends(get_db),
+                       current_user=Depends(role_required([1]))):
      try:
           bgtask.add_task(send_invoice_email,data,db)
           return {"message":"Payment mail sent succesfully"}
@@ -29,5 +31,14 @@ async def payment_mail(data:Paymentmail,bgtask:BackgroundTasks,db:Session = Depe
 #this is for view all user 
 
 @router.get("/get_userby_batch/{batch_id}")
-async def get_userby_batch(batch_id,db: Session = Depends(get_db)):
+async def get_userby_batch(batch_id,db: Session = Depends(get_db),
+                           current_user=Depends(role_required([1]))):
      return UserServices(db,None).get_usersby_batch(batch_id)
+
+@router.delete("/users/{user_id}")
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user=Depends(role_required([1]))  #only admin
+):
+    return UserServices(db, None).soft_delete_user(user_id)
