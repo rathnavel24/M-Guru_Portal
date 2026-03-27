1de33371dc469c3f3cc4b168c039f27bf3dfd534
hariprasad <hariprasad.alagesan@gmail.com>
2026-03-27 17:21:29 +0530

    hp

--- backend/app/app/crud/email_services.py
+++ backend/app/app/crud/email_services.py
@@ -48,7 +48,8 @@ def generate_invoice_id(user_id):
 
 
 async def send_invoice_email(data: Paymentmail, current_user ,db: Session):
-    sender_id =current_user["user_id"] 
+    sender_id =current_user["user_id"]
+
     try:
         user = (
             db.query(Users.username, Users.email)
@@ -107,7 +108,7 @@ async def send_invoice_email(data: Paymentmail, current_user ,db: Session):
             from_id= sender_id,
             to_id=data.user_id,
             note=data.note,
-            email_type="REMAINDER",
+            email_type=data.email_type,
             amount=data.amount,
             due_date=data.due_date,
             upi_id=data.upi_id,
@@ -117,6 +118,7 @@ async def send_invoice_email(data: Paymentmail, current_user ,db: Session):
             updated_at=datetime.now(),
             created_by="ADMIN",
         )
+
         db.add(add_log)
         db.commit()
 
