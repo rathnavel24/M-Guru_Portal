79491a7fb0d84516ef71ad2e1fa0642465621800
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-03-31 11:25:13 +0530

    null constraint

--- backend/app/app/crud/Exam_attempt_crud.py
+++ backend/app/app/crud/Exam_attempt_crud.py
@@ -323,8 +323,8 @@ class AttemptCrud:
             return {
                 "attempt_id": attempt.attempt_id,
                 "status": "in_progress",
-                "aptitude_score": attempt.aptitude_score or 0,
-                "technical_score": attempt.technical_score or 0,
+                "aptitude_score": attempt.aptitude_score or None,
+                "technical_score": attempt.technical_score or None,
                 "message": "Test is in progress"
             }
 
