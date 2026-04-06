dd179cb12e50372e51573321de31315b3f143b46
rathnavel24 <121199806+rathnavel24@users.noreply.github.com>
2026-04-06 13:50:10 +0530

    task restrictions

--- backend/app/app/schemas/task_schema.py
+++ backend/app/app/schemas/task_schema.py
@@ -14,3 +14,8 @@ class updateTask(BaseModel):
     title: Optional[str] = None
     status: Optional[int] = None
     due_time: Optional[FutureDatetime] = None
+
+
+class editTaskDetails(BaseModel):
+    title: Optional[str] = None
+    due_time: Optional[FutureDatetime] = None
