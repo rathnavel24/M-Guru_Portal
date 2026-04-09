42caf98b23bea226313c84c9f068bcb4e6dcd9cf
Keerthi <keerthikk0302@gmail.com>
2026-04-09 11:41:23 +0530

    update mentor

--- backend/app/app/api/endpoints/mentors.py
+++ backend/app/app/api/endpoints/mentors.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from backend.app.app.crud.get_allmentor import Getall_mentor
 from backend.app.app.api.deps import get_db, role_required
 from fastapi import Request
-from backend.app.app.schemas.user_schema import MentorCreate
+from backend.app.app.schemas.user_schema import MentorCreate, MentorUpdate, UserUpdate
 from backend.app.app.crud.get_allmentor import Add_mentor
 
 router = APIRouter(tags=["Mentor"])
@@ -26,6 +26,15 @@ def delete_mentor(
     mentor_service = Getall_mentor(db)
     return mentor_service.delete_mentor(mentor_id)
 
+@router.put("/update_mentor")
+def update_mentor(
+    mentor_id: int,
+    payload: MentorUpdate,
+    current_user = Depends(role_required([1])),
+    db: Session = Depends(get_db)
+):
+    mentor_service = Getall_mentor(db)
+    return mentor_service.update_mentor(mentor_id, payload)
 
 @router.post("/add_mentor")
 def add_mentor(
